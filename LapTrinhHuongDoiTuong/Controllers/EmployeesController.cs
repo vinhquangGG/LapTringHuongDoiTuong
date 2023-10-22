4ff556dc1bf4eb9549b6a3b00d15d59dca60f2f2
Syphung97 <syphung97@gmail.com>
2023-10-22 21:18:22 +0700

    Cau truc thu muc

---
+++ LapTrinhHuongDoiTuong/Controllers/EmployeesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LapTrinhHuongDoiTuong.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+
+    }
+}
