d17388859e14baf794d662906ec5009958042f21
vinhquangGG <97783811+vinhquangGG@users.noreply.github.com>
2023-10-23 14:00:06 +0700

    Update EmployeesController.cs

--- LapTrinhHuongDoiTuong/Controllers/EmployeesController.cs
+++ LapTrinhHuongDoiTuong/Controllers/EmployeesController.cs
@@ -7,6 +7,38 @@ namespace LapTrinhHuongDoiTuong.Controllers
     [ApiController]
     public class EmployeesController : ControllerBase
     {
-
+         #region Field
+        private IEmployeeBL _employeeBL;
+        #endregion
+        #region Property
+        #endregion
+        #region Contructer
+        public EmployeesController(IEmployeeBL employeeBL)
+        {
+            _employeeBL = employeeBL;
+        }
+        /// <summary>
+        /// Lấy tất cả nhân viên
+        /// </summary>
+        /// <returns>Danh sách tất cả nhân viên</returns>
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            try
+            {
+                var employees = _baseBL.GetAllRecords();
+                return StatusCode(StatusCodes.Status200OK, employees);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, new ErrorResult(
+                AmisErrorCode.Exception,
+                Resource.DevMsg_Exception,
+                Resource.UserMsg_Exception,
+                Resource.MoreInfo_InsertFailed,
+                HttpContext.TraceIdentifier));
+            }
+        }
     }
 }
